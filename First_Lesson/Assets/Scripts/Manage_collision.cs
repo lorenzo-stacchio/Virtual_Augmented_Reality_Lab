28f220320d4e05c87d3e0c9b175b0e066576671f
Lorenzo Stacchio <lorenzo.stacchio.dev@gmail.com>
2021-02-22 23:56:16 +0100

    Load first examples

---
+++ First_Lesson/Assets/Scripts/Manage_collision.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manage_collision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fetch the GameObject's Collider (make sure they have a Collider component)
+        var m_ObjectCollider = GetComponent<BoxCollider>();
+        //Here the GameObject's Collider is not a trigger
+        m_ObjectCollider.isTrigger = false;
+        //Output whether the Collider is a trigger type Collider or not
+        Debug.Log("Trigger On : " + m_ObjectCollider.isTrigger);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
