7ea7cdddc9687c0eef5b61d9c21a5d55c01d1e40
Lorenzo Stacchio <lorenzo.stacchio.dev@gmail.com>
2021-03-03 16:43:40 +0100

    Update code for lesson 1

--- First_Lesson/Assets/Scripts/Manage_collision.cs
+++ First_Lesson/Assets/Scripts/Manage_collision.cs
@@ -4,20 +4,54 @@ using UnityEngine;
 
 public class Manage_collision : MonoBehaviour
 {
+    //Declare variable to manipulate the material of the object
+    MeshRenderer thisRenderer; 
+    Vector3 startPosition;
+    float velocity = 1.0E-0f;
+    int left_right;
     // Start is called before the first frame update
+
     void Start()
     {
+        //init the render responsible for this object
+        thisRenderer = GetComponent<MeshRenderer>();
+        startPosition = this.transform.position;
         //Fetch the GameObject's Collider (make sure they have a Collider component)
-        var m_ObjectCollider = GetComponent<BoxCollider>();
+        var m_ObjectCollider = GetComponent<SphereCollider>();
         //Here the GameObject's Collider is not a trigger
         m_ObjectCollider.isTrigger = false;
-        //Output whether the Collider is a trigger type Collider or not
-        Debug.Log("Trigger On : " + m_ObjectCollider.isTrigger);
+        //decide if the ball will move left or right on the x axis
+        left_right = (Random.Range(0.0f,1.0f) < 0.5 ? -1 : 1);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+
+   private void Update() {
+       //change object position
+       Vector3 oldPosition = this.transform.position;
+       Debug.Log(oldPosition[0]);
+       this.transform.position = new Vector3(oldPosition[0] + left_right*Time.deltaTime*velocity, oldPosition[1] , oldPosition[2]);
+   }
+
+
+    private void OnTriggerEnter(Collider other) {
+       Debug.Log("Change color of object collided with us"); 
+       Material new_material = new Material(thisRenderer.material);
+       Color new_color = getRandomColor(); 
+       new_material.SetColor("_Color",  new_color);
+       thisRenderer.material = new_material;
+      
     }
+
+
+    private Color getRandomColor(){
+       Color randomColor = new Color(Random.Range(0.0f,1.0f),
+                                    Random.Range(0.0f,1.0f),
+                                    Random.Range(0.0f,1.0f));
+        return randomColor;
+   }
+
+   public Vector3 getStartPosition(){
+      return startPosition;
+   }
+    
 }
---
+++ First_Lesson/Assets/Scripts/New_Cube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class New_Cube : MonoBehaviour
+{
+
+   //dichiariamo oggetto play sphere prefab
+   public GameObject sphereToSpawn;
+
+   private void OnTriggerEnter(Collider other) {
+      Debug.Log("A sphere touch the ground");
+      Vector3 spawnPosition = other.GetComponent<Manage_collision>().getStartPosition();
+      Quaternion spawnRotation = Quaternion.Euler(Vector3.zero); 
+      GameObject spawnedSphere = Instantiate(sphereToSpawn,spawnPosition, spawnRotation);
+   }
+
+
+}
